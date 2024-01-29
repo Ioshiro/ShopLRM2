72af294b4bb88537de5e3c2f016e073677ef5921
pynopyno <magnanimog@gmail.com>
2024-01-29 12:47:53 +0100

    cambiati prezzi

--- media/lua/shared/ShopLRMItems/LRMRicambi.lua
+++ media/lua/shared/ShopLRMItems/LRMRicambi.lua
@@ -1,14 +1,43 @@
 Shop.Items["Base.LightBulb"] = {tab = Tab.Ricambi, price = 15}
-Shop.Items["Base.FrontWindow1"] = {tab = Tab.Ricambi, price = 150}
-Shop.Items["Base.FrontWindow2"] = {tab = Tab.Ricambi, price = 150}
-Shop.Items["Base.FrontWindow3"] = {tab = Tab.Ricambi, price = 150}
+Shop.Items["Base.SheetMetal"] = {tab = Tab.Ricambi, price = 50}
+Shop.Items["Base.ModernCarMuffler1"] = {tab = Tab.Ricambi, price = 70}
+Shop.Items["Base.ModernCarMuffler2"] = {tab = Tab.Ricambi, price = 70}
+Shop.Items["Base.ModernCarMuffler3"] = {tab = Tab.Ricambi, price = 70}
+Shop.Items["Base.NormalCarMuffler1"] = {tab = Tab.Ricambi, price = 70}
+Shop.Items["Base.NormalCarMuffler2"] = {tab = Tab.Ricambi, price = 70}
+Shop.Items["Base.NormalCarMuffler3"] = {tab = Tab.Ricambi, price = 70}
+Shop.Items["Base.OldCarMuffler1"] = {tab = Tab.Ricambi, price = 70}
+Shop.Items["Base.OldCarMuffler2"] = {tab = Tab.Ricambi, price = 70}
+Shop.Items["Base.OldCarMuffler3"] = {tab = Tab.Ricambi, price = 70}
+
+Shop.Items["Base.NNormalCarSeat1"] = {tab = Tab.Ricambi, price = 80}
+Shop.Items["Base.NNormalCarSeat2"] = {tab = Tab.Ricambi, price = 80}
+Shop.Items["Base.NNormalCarSeat3"] = {tab = Tab.Ricambi, price = 80}
+
+
+Shop.Items["Base.FrontWindow1"] = {tab = Tab.Ricambi, price = 160}
+Shop.Items["Base.FrontWindow2"] = {tab = Tab.Ricambi, price = 160}
+Shop.Items["Base.FrontWindow3"] = {tab = Tab.Ricambi, price = 160}
+Shop.Items["Base.ModernTire1"] = {tab = Tab.Ricambi, price = 165}
+Shop.Items["Base.ModernTire2"] = {tab = Tab.Ricambi, price = 165}
+Shop.Items["Base.ModernTire3"] = {tab = Tab.Ricambi, price = 165}
+Shop.Items["Base.NormalTire1"] = {tab = Tab.Ricambi, price = 165}
+Shop.Items["Base.NormalTire2"] = {tab = Tab.Ricambi, price = 165}
+Shop.Items["Base.NormalTire3"] = {tab = Tab.Ricambi, price = 165}
+
+Shop.Items["Base.TrunkDoor1"] = {tab = Tab.Ricambi, price = 200}
+Shop.Items["Base.TrunkDoor2"] = {tab = Tab.Ricambi, price = 200}
+Shop.Items["Base.TrunkDoor3"] = {tab = Tab.Ricambi, price = 200}
+
+Shop.Items["Base.CarBattery1"] = {tab = Tab.Ricambi, price = 200}
+Shop.Items["Base.CarBattery2"] = {tab = Tab.Ricambi, price = 200}
+Shop.Items["Base.CarBattery3"] = {tab = Tab.Ricambi, price = 200}
+
 Shop.Items["Base.Windshield1"] = {tab = Tab.Ricambi, price = 250}
 Shop.Items["Base.Windshield2"] = {tab = Tab.Ricambi, price = 250}
 Shop.Items["Base.Windshield3"] = {tab = Tab.Ricambi, price = 250}
 Shop.Items["Base.EngineDoor1"] = {tab = Tab.Ricambi, price = 250}
 Shop.Items["Base.EngineDoor2"] = {tab = Tab.Ricambi, price = 250}
 Shop.Items["Base.EngineDoor3"] = {tab = Tab.Ricambi, price = 250}
-Shop.Items["Base.CarBattery1"] = {tab = Tab.Ricambi, price = 150}
-Shop.Items["Base.CarBattery2"] = {tab = Tab.Ricambi, price = 150}
-Shop.Items["Base.CarBattery3"] = {tab = Tab.Ricambi, price = 150}
+
 Shop.Items["Base.CarBatteryCharger"] = {tab = Tab.Ricambi, price = 3000}
--- media/lua/shared/ShopLRMItems/LRMSopravvivenza.lua
+++ media/lua/shared/ShopLRMItems/LRMSopravvivenza.lua
@@ -1,5 +1,6 @@
 Shop.Items["Base.Battery"] = { tab = Tab.Sopravvivenza, price = 4}
 Shop.Items["Base.Scissors"] = { tab = Tab.Sopravvivenza, price = 25}
+Shop.Items["Base.HandTorch"] = { tab = Tab.Sopravvivenza, price = 25}
 Shop.Items["Base.EmptyPetrolCan"] = { tab = Tab.Sopravvivenza, price = 25}
 Shop.Items["Base.Pot"] = {tab = Tab.Sopravvivenza, price = 30}
 Shop.Items["Base.Lighter"] = {tab = Tab.Sopravvivenza, price = 10}
@@ -7,7 +8,9 @@ Shop.Items["Base.NailsBox"] = {tab = Tab.Sopravvivenza, price = 25}
 Shop.Items["Base.TinOpener"] = {tab = Tab.Sopravvivenza, price = 50}
 Shop.Items["Base.GardenSaw"] = {tab = Tab.Sopravvivenza, price = 250}
 Shop.Items["Base.Twine"] = {tab = Tab.Sopravvivenza, price = 400}
-Shop.Items["Base.EmptyCAN6"] = {tab = Tab.Sopravvivenza, price = 500}
-Shop.Items["Base.EmptyCAN4"] = {tab = Tab.Sopravvivenza, price = 4000}
 Shop.Items["Base.GardenSaw"] = {tab = Tab.Sopravvivenza, price = 250}
+Shop.Items["Base.EmptyCAN6"] = {tab = Tab.Sopravvivenza, price = 500}
+Shop.Items["Base.Needle"] = {tab = Tab.Sopravvivenza, price = 2000}
+Shop.Items["Base.EmptyCAN4"] = {tab = Tab.Sopravvivenza, price = 4000}
+
 
