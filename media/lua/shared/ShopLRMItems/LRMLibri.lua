9fad79d06537f0a266ef31c74fd3b31893353d14
pynopyno <magnanimog@gmail.com>
2024-02-05 12:45:50 +0100

    Cambiata ricetta market e cambiati alcuni prezzi (libri, tolto orbo e cambiato pezzi di ricampbio)

--- media/lua/shared/ShopLRMItems/LRMLibri.lua
+++ media/lua/shared/ShopLRMItems/LRMLibri.lua
@@ -1,71 +1,71 @@
-Shop.Items["Base.BookCarpentry1"] = { tab = Tab.Libri, price = 20}
-Shop.Items["Base.BookCarpentry2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookCarpentry3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookCarpentry4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookCarpentry5"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookCarpentry1"] = { tab = Tab.Libri, price = 30}
+Shop.Items["Base.BookCarpentry2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookCarpentry3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookCarpentry4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookCarpentry5"] = { tab = Tab.Libri, price = 80 }
 
-Shop.Items["Base.BookCooking1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookCooking2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookCooking3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookCooking4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookCooking5"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookCooking1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookCooking2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookCooking3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookCooking4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookCooking5"] = { tab = Tab.Libri, price = 80 }
 
-Shop.Items["Base.BookFarming1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookFarming2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookFarming3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookFarming4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookFarming5"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookFarming1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookFarming2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookFarming3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookFarming4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookFarming5"] = { tab = Tab.Libri, price = 80 }
 
-Shop.Items["Base.BookFirstAid1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookFirstAid2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookFirstAid3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookFirstAid4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookFirstAid5"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookFirstAid1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookFirstAid2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookFirstAid3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookFirstAid4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookFirstAid5"] = { tab = Tab.Libri, price = 80 }
 
-Shop.Items["Base.BookElectrician1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookElectrician2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookElectrician3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookElectrician4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookElectrician5"] = { tab = Tab.Libri, price = 50 }
-
-
-Shop.Items["Base.BookMetalWelding1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookMetalWelding2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookMetalWelding3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookMetalWelding4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookMetalWelding5"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookElectrician1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookElectrician2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookElectrician3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookElectrician4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookElectrician5"] = { tab = Tab.Libri, price = 80 }
 
 
-Shop.Items["Base.BookMechanic1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookMechanic2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookMechanic3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookMechanic4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookMechanic5"] = { tab = Tab.Libri, price = 50 }
-
-Shop.Items["Base.BookTailoring1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookTailoring2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookTailoring3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookTailoring4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookTailoring5"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookMetalWelding1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookMetalWelding2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookMetalWelding3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookMetalWelding4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookMetalWelding5"] = { tab = Tab.Libri, price = 80 }
 
 
-Shop.Items["Base.BookFishing1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookFishing2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookFishing3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookFishing4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookFishing5"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookMechanic1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookMechanic2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookMechanic3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookMechanic4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookMechanic5"] = { tab = Tab.Libri, price = 80 }
 
-Shop.Items["Base.BookTrapping1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookTrapping2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookTrapping3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookTrapping4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookTrapping5"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookTailoring1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookTailoring2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookTailoring3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookTailoring4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookTailoring5"] = { tab = Tab.Libri, price = 80 }
 
-Shop.Items["Base.BookForaging1"] = { tab = Tab.Libri, price = 20 }
-Shop.Items["Base.BookForaging2"] = { tab = Tab.Libri, price = 25 }
-Shop.Items["Base.BookForaging3"] = { tab = Tab.Libri, price = 30 }
-Shop.Items["Base.BookForaging4"] = { tab = Tab.Libri, price = 40 }
-Shop.Items["Base.BookForaging5"] = { tab = Tab.Libri, price = 50 }
+
+Shop.Items["Base.BookFishing1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookFishing2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookFishing3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookFishing4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookFishing5"] = { tab = Tab.Libri, price = 80 }
+
+Shop.Items["Base.BookTrapping1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookTrapping2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookTrapping3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookTrapping4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookTrapping5"] = { tab = Tab.Libri, price = 80 }
+
+Shop.Items["Base.BookForaging1"] = { tab = Tab.Libri, price = 30 }
+Shop.Items["Base.BookForaging2"] = { tab = Tab.Libri, price = 40 }
+Shop.Items["Base.BookForaging3"] = { tab = Tab.Libri, price = 50 }
+Shop.Items["Base.BookForaging4"] = { tab = Tab.Libri, price = 60 }
+Shop.Items["Base.BookForaging5"] = { tab = Tab.Libri, price = 80 }
 
 
 
--- media/lua/shared/ShopLRMItems/LRMRicambi.lua
+++ media/lua/shared/ShopLRMItems/LRMRicambi.lua
@@ -1,11 +1,11 @@
 Shop.Items["Base.LightBulb"] = {tab = Tab.Ricambi, price = 15}
 Shop.Items["Base.SheetMetal"] = {tab = Tab.Ricambi, price = 50}
-Shop.Items["Base.ModernCarMuffler1"] = {tab = Tab.Ricambi, price = 70}
-Shop.Items["Base.ModernCarMuffler2"] = {tab = Tab.Ricambi, price = 70}
-Shop.Items["Base.ModernCarMuffler3"] = {tab = Tab.Ricambi, price = 70}
-Shop.Items["Base.NormalCarMuffler1"] = {tab = Tab.Ricambi, price = 70}
-Shop.Items["Base.NormalCarMuffler2"] = {tab = Tab.Ricambi, price = 70}
-Shop.Items["Base.NormalCarMuffler3"] = {tab = Tab.Ricambi, price = 70}
+Shop.Items["Base.ModernCarMuffler1"] = {tab = Tab.Ricambi, price = 120}
+Shop.Items["Base.ModernCarMuffler2"] = {tab = Tab.Ricambi, price = 120}
+Shop.Items["Base.ModernCarMuffler3"] = {tab = Tab.Ricambi, price = 120}
+Shop.Items["Base.NormalCarMuffler1"] = {tab = Tab.Ricambi, price = 90}
+Shop.Items["Base.NormalCarMuffler2"] = {tab = Tab.Ricambi, price = 90}
+Shop.Items["Base.NormalCarMuffler3"] = {tab = Tab.Ricambi, price = 90}
 Shop.Items["Base.OldCarMuffler1"] = {tab = Tab.Ricambi, price = 70}
 Shop.Items["Base.OldCarMuffler2"] = {tab = Tab.Ricambi, price = 70}
 Shop.Items["Base.OldCarMuffler3"] = {tab = Tab.Ricambi, price = 70}
@@ -21,9 +21,11 @@ Shop.Items["Base.FrontWindow3"] = {tab = Tab.Ricambi, price = 160}
 Shop.Items["Base.ModernTire1"] = {tab = Tab.Ricambi, price = 165}
 Shop.Items["Base.ModernTire2"] = {tab = Tab.Ricambi, price = 165}
 Shop.Items["Base.ModernTire3"] = {tab = Tab.Ricambi, price = 165}
-Shop.Items["Base.NormalTire1"] = {tab = Tab.Ricambi, price = 165}
-Shop.Items["Base.NormalTire2"] = {tab = Tab.Ricambi, price = 165}
-Shop.Items["Base.NormalTire3"] = {tab = Tab.Ricambi, price = 165}
+Shop.Items["Base.NormalTire1"] = {tab = Tab.Ricambi, price = 135}
+Shop.Items["Base.NormalTire2"] = {tab = Tab.Ricambi, price = 135}
+Shop.Items["Base.NormalTire3"] = {tab = Tab.Ricambi, price = 135}
+
+
 
 Shop.Items["Base.TrunkDoor1"] = {tab = Tab.Ricambi, price = 200}
 Shop.Items["Base.TrunkDoor2"] = {tab = Tab.Ricambi, price = 200}
