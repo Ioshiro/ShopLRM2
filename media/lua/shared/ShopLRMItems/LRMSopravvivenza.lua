24816d0b61290c6104ccc10250e2366df79ffb0c
pynopyno <magnanimog@gmail.com>
2024-03-04 11:57:42 +0100

    cambiato prezzo tanica  media

--- media/lua/shared/ShopLRMItems/LRMSopravvivenza.lua
+++ media/lua/shared/ShopLRMItems/LRMSopravvivenza.lua
@@ -7,8 +7,8 @@ Shop.Items["Base.Lighter"] = {tab = Tab.Sopravvivenza, price = 15}
 Shop.Items["Base.NailsBox"] = {tab = Tab.Sopravvivenza, price = 25}
 Shop.Items["Base.TinOpener"] = {tab = Tab.Sopravvivenza, price = 80}
 Shop.Items["Base.GardenSaw"] = {tab = Tab.Sopravvivenza, price = 250}
-Shop.Items["Base.Twine"] = {tab = Tab.Sopravvivenza, price = 500}
-Shop.Items["Base.EmptyCAN6"] = {tab = Tab.Sopravvivenza, price = 500}
+Shop.Items["Base.Twine"] = {tab = Tab.Sopravvivenza, price = 400}
+Shop.Items["Base.EmptyCAN6"] = {tab = Tab.Sopravvivenza, price = 1500}
 Shop.Items["Base.Needle"] = {tab = Tab.Sopravvivenza, price = 2000}
 Shop.Items["Base.EmptyCAN4"] = {tab = Tab.Sopravvivenza, price = 4000}
 
